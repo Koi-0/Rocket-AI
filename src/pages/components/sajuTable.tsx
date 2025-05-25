43f02be5988dedc8c53d98774bda691876f36b2f
Koi-0 <dndud5182@gmail.com>
2025-05-25 21:35:42 +0900

    Refactor : SajuTable 컴포넌트에서 불필요한 스타일 및 렌더링 로직 제거, SajuRow 컴포넌트로 분리

--- src/pages/components/sajuTable.tsx
+++ src/pages/components/sajuTable.tsx
@@ -1,105 +1,14 @@
 import React from "react";
 import { SajuCell } from "../types/sajuType";
 import { sajuTableData } from "../data/sajuTableData";
+import SajuRow from "./sajuRow";
 
 const SajuTable = () => {
-  // 특정 cell에 대한 스타일
-  const getSajuCellStyle = (rowIndex: number, colIndex: number) => {
-    if (rowIndex === 1 && colIndex === 2) {
-      return {
-        backgroundColor: "#C23030",
-      };
-    }
-
-    if (rowIndex === 2 && colIndex === 1) {
-      return {
-        backgroundColor: "#18868C",
-      };
-    }
-
-    if (rowIndex === 2 && colIndex === 2) {
-      return {
-        backgroundColor: "#C23030",
-      };
-    }
-
-    if (rowIndex === 2 && colIndex === 4) {
-      return {
-        backgroundColor: "#f9f9f9",
-        border: "1px solid #000000",
-        color: "#000000",
-      };
-    }
-
-    return {};
-  };
-
   // 특정 cell에 대한 오른쪽 테두리 스타일
   const getBorderRightStyle = (colIndex: number) => {
     return colIndex >= 1 && colIndex <= 3
       ? "0.5px solid #8A8A8A"
       : "1px solid #000000";
-  };
-
-  // 각 cell의 내용을 렌더링하는 함수
-  const renderCellContent = (
-    cell: SajuCell,
-    rowIndex: number,
-    colIndex: number,
-  ) => {
-    const firstCol = colIndex === 0;
-    const specialRow = rowIndex === 1 || rowIndex === 2;
-    const specialCol = colIndex >= 1 && colIndex <= 4;
-    const specialCell = specialRow && specialCol;
-
-    // cell이 배열인 경우
-    if (Array.isArray(cell)) {
-      return (
-        <div
-          className={`m-1 flex flex-col items-center justify-center gap-1 leading-tight`}
-        >
-          {cell.map((item, i) => (
-            <div key={i} className="flex flex-col items-center">
-              <div className="text-sm">{item.main}</div>
-              <div className="text-[9px]">{item.sub1}</div>
-            </div>
-          ))}
-        </div>
-      );
-    }
-
-    return (
-      <div
-        className={`flex flex-col items-center justify-center text-center leading-tight ${
-          specialCell
-            ? "m-1 h-[55px] rounded-xl bg-[#2f2f2f] text-white"
-            : "h-[42px]"
-        } ${firstCol ? "w-12" : "w-[55px]"}`}
-        style={getSajuCellStyle(rowIndex, colIndex)}
-      >
-        <div
-          className={
-            firstCol ? "text-[12px]" : specialCell ? "text-[7px]" : "text-sm"
-          }
-        >
-          {cell.main}
-        </div>
-        <div
-          className={
-            firstCol
-              ? "text-[7px]"
-              : specialCell
-                ? "text-[25px]"
-                : "0 text-[10px]"
-          }
-        >
-          {cell.sub1}
-        </div>
-        {cell.sub2 && (
-          <div className={specialCell ? "text-[8px]" : ""}>{cell.sub2}</div>
-        )}
-      </div>
-    );
   };
 
   return (
@@ -125,24 +34,14 @@ const SajuTable = () => {
               })}
             </tr>
           </thead>
-
           <tbody>
             {sajuTableData.rows.map((rowData, rowIndex) => (
-              <tr key={`rowData-${rowIndex}`}>
-                {rowData.map((cellData, colIndex) => {
-                  return (
-                    <td
-                      key={`cellData-${rowIndex}-${colIndex}`}
-                      className="border-b border-b-black"
-                      style={{ borderRight: getBorderRightStyle(colIndex) }}
-                    >
-                      <div className="flex flex-col items-center justify-center">
-                        {renderCellContent(cellData, rowIndex, colIndex)}
-                      </div>
-                    </td>
-                  );
-                })}
-              </tr>
+              <SajuRow
+                key={`rowData-${rowIndex}`}
+                rowData={rowData}
+                rowIndex={rowIndex}
+                getBorderRightStyle={getBorderRightStyle}
+              />
             ))}
           </tbody>
         </table>
