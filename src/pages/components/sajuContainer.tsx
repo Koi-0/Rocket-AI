fa692e4dddc9897432d503f3246ba430c037397a
Koi-0 <dndud5182@gmail.com>
2025-05-25 21:14:45 +0900

    Feat : SajuContainer 컴포넌트 추가

---
+++ src/pages/components/sajuContainer.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import SajuTable from "./sajuTable";
+
+const SajuContainer = () => {
+  return (
+    <div className="absolute bottom-20 left-1/2 h-[621px] w-[351px] translate-x-[-50%]">
+      <Image
+        src={"/saju.image.png"}
+        alt="사주팔자 배경 이미지"
+        fill
+        style={{ objectFit: "contain" }}
+      />
+      <SajuTable />
+    </div>
+  );
+};
+
+export default SajuContainer;
